ddafa1651b98a2153b6f8acca80eb54350219056
Mukherjee <arnab.mukherjee@hpe.com>
2023-09-01 11:51:05 +0530

    migration init

---
+++ cray_product_catalog/migration/kube_apis.py
@@ -0,0 +1,21 @@
+
+
+def create_config_map(api_instance, name, namespace):
+    pass
+
+
+def read_config_map(api_instance, name, namespace):
+    pass
+
+
+def delete_config_map(api_instance, name, namespace):
+    pass
+
+
+def rename_config_map(api_instance, name_new, name_old, namespace):
+    pass
+
+
+def update_service_account(api_instance, name, namespace, action: str, is_grant: bool):
+    pass
+
