bab6191522bd6b7e9d238a3942d1bb04e7977a78
U-ASIAPACIFIC\nanjundl <nanjundl@KQFCSS3YC4.asiapacific.hpqcorp.net>
2023-09-06 15:08:20 +0530

    Initial change to handle config map data split into multiple configmaps

--- cray_product_catalog/migration/kube_apis.py
+++ cray_product_catalog/migration/kube_apis.py
@@ -1,21 +1,55 @@
+#
+# MIT License
+#
+# (C) Copyright 2023 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+
+"""
+Kubernets API
+"""
+
+class KubernetesApi:
+    def __init__(self) -> None:
+        pass
+
+    def create_config_map(self, name, namespace, data, label):
+        pass
 
 
-def create_config_map(api_instance, name, namespace):
-    pass
+    def read_config_map(self, name, namespace):
+        pass
 
 
-def read_config_map(api_instance, name, namespace):
-    pass
+    def delete_config_map(self, name, namespace):
+        pass
 
 
-def delete_config_map(api_instance, name, namespace):
-    pass
+    def rename_config_map(self, name_new, name_old, namespace):
+        pass
 
 
-def rename_config_map(api_instance, name_new, name_old, namespace):
-    pass
+    def update_service_account(self, name, namespace, action: str, is_grant: bool):
+        pass
 
+    def rename_config_map(self, old_name, new_name, label):
+        pass
 
-def update_service_account(api_instance, name, namespace, action: str, is_grant: bool):
-    pass
 
